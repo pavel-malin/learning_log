ce042d4cc4715aceff11c18733ab5db4f506b6dd
pavel-malin <kurchevskijpavel@gmail.com>
2019-05-20 17:03:18 +0300

    Ready for deployment to heroku.

--- .gitignore/11_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/root/Загрузки/PPD/python_crash_course/learning_log/11_env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
